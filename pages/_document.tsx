5d07c5583f0da6c785016869ba74b8f68a933037
Ryan Callahan <RyanCallahan312@gmail.com>
2022-05-30 17:46:42 -0700

    setup google analytics and commented gtag
    leaving gtag commented for later if I need it. not sure rn

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,40 @@
 import { css } from "@emotion/react";
+import Script from "next/dist/client/script";
 import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
 	return (
 		<Html>
 			<Head>
+				{/*<!-- Google Tag Manager -->*/}
+				{/* <Script
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+						j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+						'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+						})(window,document,'script','dataLayer','GTM-K4LFZ2N');`
+					}}
+				/> */}
+				{/* <!-- End Google Tag Manager --> */}
+				{/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+				<Script
+					strategy="afterInteractive"
+					src="https://www.googletagmanager.com/gtag/js?id=G-DPB94Z1YV5"
+				/>
+				<Script
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+
+						gtag('config', 'G-DPB94Z1YV5');`
+					}}
+				/>
+				{/* <!-- End Global site tag (gtag.js) - Google Analytics --> */}
 				<link rel="shortcut icon" href="/DevRyanLogo.svg" />
 				<link rel="canonical" href="https://blog.devryan.io/" />
 				<meta name="language" content="en" />
@@ -17,6 +47,14 @@ export default function Document() {
 				height: 100%;
 				background-color: #252834;
 			`}>
+				{/* <!-- Google Tag Manager (noscript) --> */}
+				{/* <noscript
+					dangerouslySetInnerHTML={{
+						__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K4LFZ2N"
+						height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+					}}
+				/> */}
+				{/* <!-- End Google Tag Manager (noscript) --> */}
 				<Main />
 				<NextScript />
 			</body>
